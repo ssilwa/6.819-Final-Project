8f8d46f2afa997d3c2a082229ea61c217e101347
Allen <allenlee@mit.edu>
2017-12-01 13:11:29 -0500

    debug

--- models/emnist_2.py
+++ models/emnist_2.py
@@ -25,10 +25,13 @@ def load_data(mat_file_path, width=28, height=28, max_=None, verbose=True):
                 - ((training_images, training_labels), (testing_images, testing_labels), mapping)
     '''
     # Local functions
-    def rotate(img):
+    def reshape(img):
         # Used to rotate images (for some reason they are transposed on read-in)
-        flipped = np.fliplr(img)
-        return np.rot90(flipped)
+        img.shape = (width,height)
+        img = img.T
+        img = list(img)
+        img = [item for sublist in img for item in sublist]
+        return img
 
     def display(img, threshold=0.5):
         # Debugging only
@@ -43,7 +46,7 @@ def load_data(mat_file_path, width=28, height=28, max_=None, verbose=True):
         return render
 
     # Load convoluted list structure form loadmat
-    mat = loadmat(mat_file_path)
+    mat = loadmat(args.file)
 
     # Load char mapping
     mapping = {kv[0]:kv[1:][0] for kv in mat['dataset'][0][0][2]}
@@ -52,7 +55,7 @@ def load_data(mat_file_path, width=28, height=28, max_=None, verbose=True):
     # Load training data
     if max_ == None:
         max_ = len(mat['dataset'][0][0][0][0][0][0])
-    training_images = mat['dataset'][0][0][0][0][0][0][:max_].reshape(max_, height, width, 1)
+    training_images = mat['dataset'][0][0][0][0][0][0][:max_]
     training_labels = mat['dataset'][0][0][0][0][0][1][:max_]
 
     # Load testing data
@@ -60,22 +63,26 @@ def load_data(mat_file_path, width=28, height=28, max_=None, verbose=True):
         max_ = len(mat['dataset'][0][0][1][0][0][0])
     else:
         max_ = int(max_ / 6)
-    testing_images = mat['dataset'][0][0][1][0][0][0][:max_].reshape(max_, height, width, 1)
+    testing_images = mat['dataset'][0][0][1][0][0][0][:max_]
     testing_labels = mat['dataset'][0][0][1][0][0][1][:max_]
 
     # Reshape training data to be valid
     if verbose == True: _len = len(training_images)
     for i in range(len(training_images)):
         if verbose == True: print('%d/%d (%.2lf%%)' % (i + 1, _len, ((i + 1)/_len) * 100), end='\r')
-        training_images[i] = rotate(training_images[i])
+        training_images[i] = reshape(training_images[i])
     if verbose == True: print('')
 
     # Reshape testing data to be valid
     if verbose == True: _len = len(testing_images)
     for i in range(len(testing_images)):
         if verbose == True: print('%d/%d (%.2lf%%)' % (i + 1, _len, ((i + 1)/_len) * 100), end='\r')
-        testing_images[i] = rotate(testing_images[i])
+        testing_images[i] = reshape(testing_images[i])
     if verbose == True: print('')
+
+    # Extend the arrays to (None, 28, 28, 1)
+    training_images = training_images.reshape(training_images.shape[0], height, width, 1)
+    testing_images = testing_images.reshape(testing_images.shape[0], height, width, 1)
 
     # Convert type to float32
     training_images = training_images.astype('float32')
@@ -85,11 +92,9 @@ def load_data(mat_file_path, width=28, height=28, max_=None, verbose=True):
     training_images /= 255
     testing_images /= 255
 
-    nb_classes = len(mapping)
+    return ((training_images, training_labels), (testing_images, testing_labels), mapping)
 
-    return ((training_images, training_labels), (testing_images, testing_labels), mapping, nb_classes)
-
-def build_net(training_data, width=28, height=28, verbose=False):
+def build_net(training_data, width=28, height=28, epochs=10, verbose=False):
     ''' Build and train neural network. Also offloads the net in .yaml and the
         weights in .h5 to the bin/.
         Arguments:
@@ -101,13 +106,19 @@ def build_net(training_data, width=28, height=28, verbose=False):
             verbose: enable verbose printing
     '''
     # Initialize data
-    (x_train, y_train), (x_test, y_test), mapping, nb_classes = training_data
+    (x_train, y_train), (x_test, y_test), mapping = training_data
     input_shape = (height, width, 1)
+    nb_classes = len(mapping)
 
     # Hyperparameters
+    batch_size = 256
     nb_filters = 32 # number of convolutional filters to use
     pool_size = (2, 2) # size of pooling area for max pooling
     kernel_size = (3, 3) # convolution kernel size
+
+    # convert class vectors to binary class matrices
+    y_train = np_utils.to_categorical(y_train, nb_classes)
+    y_test = np_utils.to_categorical(y_test, nb_classes)
 
     model = Sequential()
     model.add(Convolution2D(nb_filters,
@@ -123,42 +134,25 @@ def build_net(training_data, width=28, height=28, verbose=False):
     model.add(Dropout(0.25))
     model.add(Flatten())
 
-    print("added 2 conv, pooling, dropout")
     model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(nb_classes, activation='softmax'))
-    print("added dense")
 
     model.compile(loss='categorical_crossentropy',
                   optimizer='adadelta',
                   metrics=['accuracy'])
 
     if verbose == True: print(model.summary())
-    return model
 
-def train(model, training_data, callback=True, batch_size=256, epochs=10):
-    print("starting train funct")
-    (x_train, y_train), (x_test, y_test), mapping, nb_classes = training_data
+    # Callback for analysis in TensorBoard
+    tbCallBack = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
 
-    # convert class vectors to binary class matrices
-    y_train = np_utils.to_categorical(y_train, nb_classes)
-    y_test = np_utils.to_categorical(y_test, nb_classes)
-
-    if callback == True:
-        # Callback for analysis in TensorBoard
-        tbCallBack = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
-
-    print("Beginning training")
-
-    print(y_train.shape)
-    print("______________")
-    print(x_train.shape)
     model.fit(x_train, y_train,
               batch_size=batch_size,
               epochs=epochs,
               verbose=1,
               validation_data=(x_test, y_test),
-              callbacks=[tbCallBack] if callback else None)
+              callbacks=[tbCallBack])
 
     score = model.evaluate(x_test, y_test, verbose=0)
     print('Test score:', score[0])
